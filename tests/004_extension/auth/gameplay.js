28ea6a186343847688e787b9e9a9448720cb07bc
1024sparrow <1024sparrow@gmail.com>
2017-12-23 16:52:42 +0300

    123

---
+++ tests/004_extension/auth/gameplay.js
@@ -0,0 +1,28 @@
+'use strict';
+/*auth*/
+
+EXTERNAL = {
+    layouts:{
+        canvas:{
+            type: 'strip',
+            orient: 'h',
+            items:[
+                {
+                    widget: 'aa'
+                },
+                {
+                    widget: 'bb'
+                },
+                {
+                    widget: 'cc'
+                }
+            ]
+        }
+    },
+    widgets:{
+    },
+    states:{
+    },
+    //extender
+    hideWidgets: true
+}
