97cd41f37cf55f85895860f66a0942028bf2beaa
boris <1024sparrow@gmail.com>
2018-01-04 05:52:18 +0300

    123

---
+++ tests/004_stateurls/gameplay.js
@@ -0,0 +1,19 @@
+'use strict';
+
+Traliva.debug = {
+    states: true,
+    url: 'traliva.ru'
+};
+Traliva.init({
+    layouts:{
+        type: 'strip',
+        orient: 'v',
+        items:[ {widget:'w1'}, {widget:'w2'} ]
+    },
+    widgets:{
+        w1:{
+            constructor: TralivaKit.Button,
+            options: {title: 'Нажмите здесь', togglable: true, valueVarName: 'enabled'}
+        }
+    }
+});
