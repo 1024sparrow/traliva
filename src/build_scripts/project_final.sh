1202a91125b0b0ceae6b2ab258906369daf3243c
1024sparrow <1024sparrow@gmail.com>
2018-07-26 14:10:23 +0300

    123

--- src/build_scripts/project_final.sh
+++ src/build_scripts/project_final.sh
@@ -9,11 +9,14 @@ source $1/$2/config
 UTILS_PATH=$(dirname "$0")/utils
 ############################
 
-stack=($1/$2)
+# Здесь мы обходим дерево директорий children и производим изменения в файлах в этом дереве
+# Формируем переменные root, list и all.
+root=$1/$2
+declare -a list # список директорий на обработку. Не включая корень
+stack=($root)
 while [ ! ${#stack[@]} -eq 0 ]
 do
     element=${stack[@]:0:1}/children
-    echo "-- $element"
     stack=("${stack[@]:1}")
     if [ -d $element ]
     then
@@ -22,32 +25,33 @@ do
             if [ -d $element/$i ]
             then
                 stack=(${stack[@]} $element/$i)
+                list=(${list[@]} $element/$i)
             fi
         done
     fi
 done
+all=($root ${list[@]})
 
-#rm -rf $1/$2/traliva $1/$2/traliva_kit
-
+############################
 # Ожидается в директориях traliva и traliva_kit найти файл style.css и директорию res
-
-#echo "$1/$2/traliva_kit:"
-#ls $1/$2/traliva_kit
-sed -e "s/#RES#/#RES#\/_traliva/g" $1/$2/traliva/style.css > $1/$2/style.css_tmp
-sed -e "s/#RES#/#RES#\/_traliva_kit/g" $1/$2/traliva_kit/style.css >> $1/$2/style.css_tmp
-##sed -e "s/#RES#/#RES#/g" $1/$2/style.css >> $1/$2/style.css_tmp
-cat $1/$2/style.css >> $1/$2/style.css_tmp
-if [ "$COMPRESS_LINEBREAKS" = true ]
-then
-    cat $1/$2/style.css_tmp | $UTILS_PATH/css.py > $1/$2/style.css && rm $1/$2/style.css_tmp
-else
-    mv $1/$2/style.css_tmp $1/$2/style.css
-fi
-
-mv $1/$2/traliva/res $1/$2/res/_traliva
-mv $1/$2/traliva_kit/res $1/$2/res/_traliva_kit
-
-#mv $1/$2/traliva/traliva.js $1/$2/ && rm -rf $1/$2/traliva
-#mv $1/$2/traliva_kit/traliva_kit.js $1/$2/ && rm -rf $1/$2/traliva_kit
-rm -rf $1/$2/traliva
-rm -rf $1/$2/traliva_kit
+sed -e "s/#RES#/#RES#\/_traliva/g" $root/traliva/style.css > $root/style.css_tmp
+mv $root/traliva/res $root/res/_traliva
+rm -rf $root/traliva
+for i in $all
+do
+    #echo "** $i"
+    if [ ! -a $i/style.css_tmp ]; then touch $i/style.css_tmp; fi
+    if [ -d $i/traliva_kit ]
+    then
+        sed -e "s/#RES#/#RES#\/_traliva_kit/g" $i/traliva_kit/style.css >> $i/style.css_tmp
+        mv $i/traliva_kit/res $i/res/_traliva_kit
+        rm -rf $i/traliva_kit
+    fi
+    cat $i/style.css >> $i/style.css_tmp
+    if [ "$COMPRESS_LINEBREAKS" = true ]
+    then
+        cat $i/style.css_tmp | $UTILS_PATH/css.py > $i/style.css && rm $i/style.css_tmp
+    else
+        mv $i/style.css_tmp $i/style.css
+    fi
+done
