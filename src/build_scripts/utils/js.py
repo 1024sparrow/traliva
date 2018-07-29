a304b5895a36673ee56f369976b6dc2e3c25d3e3
1024sparrow <1024sparrow@gmail.com>
2018-07-29 05:50:31 +0300

    123

--- src/build_scripts/utils/js.py
+++ src/build_scripts/utils/js.py
@@ -30,7 +30,27 @@ a = re.sub(re.compile(r'\s*}\s*', re.DOTALL), '}', a)
 #a = re.sub(re.compile(r'//.*\n', re.DOTALL), '', a)
 b = ''
 in_comment = False
-for index in range(0, len(a)):
-    b += a[index]
+in_string_1 = False # '
+in_string_2 = False # "
+in_string = False # boris here
+prev_char = 's' # nor '\\' or '/' or '*'
+for i in a:
+    skip_current = False
+    if prev_char == '/' and i == '*':
+        in_comment = True
+        b = b[:-1] # удаляем предыдущий символ ('/')
+    elif prev_char == '*' and i == '/':
+        in_comment = False
+        skip_current = True
+    elif prev_char != '\\' and i == '"':
+        in_string = not in_string
+    if not in_comment and not skip_current:
+        if in_string:
+            b += i
+        else:
+            b += i #
+    #else:
+    #    b += '%'
+    prev_char = i
 print(b)
 
