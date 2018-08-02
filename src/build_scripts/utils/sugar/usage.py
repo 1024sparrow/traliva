986e067c76c5c11c8b55919d9b973166cc8578fc
1024sparrow <1024sparrow@gmail.com>
2018-08-02 05:52:06 +0300

    usage.py: успешное определение активированных идентификаторов (и удаление записей активации)

--- src/build_scripts/utils/sugar/usage.py
+++ src/build_scripts/utils/sugar/usage.py
@@ -14,33 +14,53 @@ def process(p_js, p_css, p_js_css):
     # ... #u#идентификатор## ...
     # 0   1234            560
     for fil in p_js_css:
-        s = 0
-        print('##')
+        #if not '1.js' in fil['filepath']:#
+        #    continue#
         for fragment in fil['text']:
-            if fragment['type'] != 1:
-                continue
-            print(fragment['text'])
-            a = '' # изменённый текст на замещение старого
-            id_cand = ''
-            t = '' # текущий фрагмент #u#...##
-            detected = False
-            for i in fragment['text']:
-                if s == 0 and i == '#':
-                    s = 1
-                elif s == 1 and i == 'u':
-                    s = 2
-                elif s == 2 and i == '#':
-                    s = 3
-                elif s == 3 and char_valid_for_id(i):
-                    s = 4
-                    id_cand = i
-                elif s == 4 and char_valid_for_id(i):
-                    id_cand += i
-                elif s == 4 and i == '#':
-                    s = 5
-                elif s == 5 and i == '#':
-                    s = 6 # <---------
-                    activated_ids.add(id_cand)
-                    id_cand = ''
-                else:
-                    s = 0
+            if fragment['type'] == 1:
+                print('#### INIT:\n', fragment['text'])
+                s = 0
+                a = '' # изменённый текст на замещение старого
+                id_cand = ''
+                t = '' # текущий фрагмент #u#...##
+                detected = False
+                for i in fragment['text']:
+                    ordinary = False
+                    if s == 0 and i == '#':
+                        s = 1
+                    elif s == 1 and i == 'u':
+                        s = 2
+                    elif s == 2 and i == '#':
+                        s = 3
+                    elif s == 3 and char_valid_for_id(i):
+                        s = 4
+                        id_cand = i
+                    elif s == 4 and char_valid_for_id(i):
+                        id_cand += i
+                    elif s == 4 and i == '#':
+                        s = 5
+                    elif s == 5 and i == '#':
+                        s = 0
+                        activated_ids.add(id_cand)
+                        id_cand = ''
+                        detected = True
+                    else:
+                        ordinary = True
+                        s = 0
+                    if ordinary:
+                        a += t + i
+                        t = ''
+                    else:
+                        if s:
+                            t += i
+                        else:
+                            if detected:
+                                detected = False
+                                id_cand = ''
+                                t = ''
+                            else:
+                                a += t
+                            t = ''
+                    #print('%s - %s' % (s, i))
+                print('#### RESULT:\n', a)
+    print('detected activated: ', activated_ids)
