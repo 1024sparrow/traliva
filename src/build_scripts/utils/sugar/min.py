0652e6c890bb1688d841765173e6950af3644a76
1024sparrow <1024sparrow@gmail.com>
2018-08-12 06:59:40 +0300

    min.py

--- src/build_scripts/utils/sugar/min.py
+++ src/build_scripts/utils/sugar/min.py
@@ -2,3 +2,28 @@
 
 def process(p_js, p_css, p_js_css):
     print('min: process()')
+    for fil in p_js:
+        for fragment in fil['text']:
+            if fragment['type'] == 1:
+                s = 3
+                a = ''
+                for i in fragment['text']:
+                    if i.isspace():
+                        cand = ' '
+                    elif i.isalpha() or i in '_$':
+                        if not s == 3:
+                            a += cand
+                        cand = ''
+                        a += i
+                        s = 1
+                    elif i.isdigit():
+                        if not s == 3:
+                            a += cand
+                        cand = ''
+                        a += i
+                        s = 2
+                    else:
+                        cand = ''
+                        a += i
+                        s = 3
+                fragment['text'] = a
