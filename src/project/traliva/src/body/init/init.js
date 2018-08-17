502ec860c43c3354711239ee2b1c6b54462227bd
VasilyevBP <1024sparrow@gmail.com>
2018-08-17 16:40:00 +0300

    123

--- src/project/traliva/src/body/init/init.js
+++ src/project/traliva/src/body/init/init.js
@@ -56,7 +56,7 @@ $Traliva.$init = function($o){
         return;
     }
     #USAGE_END#debug##
-    $Traliva.$widgets = {};//сюда будут записываться указатели на сгенерированые виджеты для доступа из кода, описанного в $o.states.stateSubscribers, по идентификатору виджета
+    $Traliva.$widgets = {};//сюда будут записываться указатели на сгенерированые виджеты для доступа из кода, описанного в $o.states.$stateSubscribers, по идентификатору виджета
     var $d = $Traliva.$__d = {};
     $d.$o = $o;
     $d.$w = {};//widgets (WidgetStateSubscriber)
@@ -65,97 +65,99 @@ $Traliva.$init = function($o){
         $Traliva.$api = {};
         $o.$initApi($o.$target, $Traliva.$api);
     }
-    $d.$logics = [];//сюда будут сохраняться экземпляры LogicsStateSubscriber, чтобы у них вызывать метод initializeGui()
+    $d.$logics = [];//сюда будут сохраняться экземпляры LogicsStateSubscriber, чтобы у них вызывать метод $initializeGui()
 
     $d.$publisher = new $StatePublisher();
     $d.$publisher.$setState($o.$states.$initState);
     if ($Traliva.$debug){
-        $d.__debug = {
-            $publisher: new StatePublisherNoDebug()
+        $d.$__debug = {
+            $publisher: new $StatePublisherNoDebug()
         };
         $3 = {
-            show_states: false
+            $show_states: false
         };
-        if ($Traliva.$debug.hasOwnProperty('url')){
-            $3.url = $Traliva.$debug.url;
+        if ($Traliva.$debug.$hasOwnProperty('$url')){
+            $3.$url = $Traliva.$debug.$url;
         }
-        $d.__debug.$publisher.$setState($3);
+        $d.$__debug.$publisher.$setState($3);
 
-        $d.__debug.$wRoot = new Strip($Traliva.Strip__Orient__vert);
-        var __wDebugPanel = new Strip($Traliva.Strip__Orient__hor, $d.__debug.$wRoot);
-            __wDebugPanel._div.className = 'traliva__debug_panel';
-            var __wDebugPanelBnStates = new Widget(__wDebugPanel);
-            __wDebugPanel.addItem(__wDebugPanelBnStates, '128px');
-            $d.__debug.$publisher.registerSubscriber(new Button(__wDebugPanelBnStates, {valueVarName: 'show_states', color: '#48f', title: 'Состояние'}));
-            if ($Traliva.$debug.hasOwnProperty('url')){
-                var __wDebugPanelUrl = new Widget(__wDebugPanel);
-                __wDebugPanel.addItem(__wDebugPanelUrl);
-                if ($o.hasOwnProperty('$states') && $o.$states.hasOwnProperty('tree'))
-                    $d.__debug.$publisher.registerSubscriber(new DebugPanelUrlWidget(__wDebugPanelUrl));
+        $d.$__debug.$wRoot = new $Strip($Traliva.$Strip__Orient__vert);
+        var $__wDebugPanel = new $Strip($Traliva.$Strip__Orient__hor, $d.$__debug.$wRoot);
+            $__wDebugPanel.$_div.className = '$traliva__debug_panel';
+            var $__wDebugPanelBnStates = new $Widget($__wDebugPanel);
+            $__wDebugPanel.$addItem($__wDebugPanelBnStates, '128px');
+            $d.$__debug.$publisher.$registerSubscriber(new $Button($__wDebugPanelBnStates, {$valueVarName: '$show_states', $color: '#48f', $title: 'Состояние'}));
+            if ($Traliva.$debug.hasOwnProperty('$url')){
+                var $__wDebugPanelUrl = new $Widget($__wDebugPanel);
+                $__wDebugPanel.$addItem($__wDebugPanelUrl);
+                if ($o.hasOwnProperty('$states') && $o.$states.hasOwnProperty('$tree'))
+                    $d.$__debug.$publisher.$registerSubscriber(new $DebugPanelUrlWidget($__wDebugPanelUrl));
                 else
-                    __wDebugPanelUrl.$setContent($Traliva.createElement('<p class="traliva__debug_panel__error">URL: дерево переходов не задано</p>'));
+                    $__wDebugPanelUrl.$setContent($Traliva.$createElement('<p class="$traliva__debug_panel__error">URL: дерево переходов не задано</p>'));
             }
-        $d.__debug.$wRoot.addItem(__wDebugPanel, '32px');
-        var __wDebugCanvas = new Stack($d.__debug.$wRoot);
-        $d.__debug.$wRoot.addItem(__wDebugCanvas);
-        $3 = new Widget(__wDebugCanvas);
-        __wDebugCanvas.addItem($3);
+        $d.$__debug.$wRoot.$addItem($__wDebugPanel, '32px');
+        var $__wDebugCanvas = new $Stack($d.$__debug.$wRoot);
+        $d.$__debug.$wRoot.$addItem($__wDebugCanvas);
+        $3 = new $Widget($__wDebugCanvas);
+        $__wDebugCanvas.$addItem($3);
         $d.$wRoot = $3;
-        if ($Traliva.$debug.hasOwnProperty('state')){
-            var __wDebugStates = new Strip($Traliva.Strip__Orient__vert, __wDebugCanvas);
-            var __wDebugStatesExtender = new Widget(__wDebugStates);
-            __wDebugStates.addItem(__wDebugStatesExtender, '64px');
-            var __wDebugStatesStates = new Widget(__wDebugStates);
-            __wDebugStates.addItem(__wDebugStatesStates);
-            $d.__debug.$publisher.registerSubscriber(new DebugStatesWidget(__wDebugStates, __wDebugStatesExtender, __wDebugStatesStates));
-            __wDebugCanvas.addItem(__wDebugStates);
+        if ($Traliva.$debug.hasOwnProperty('$state')){
+            var $__wDebugStates = new $Strip($Traliva.$Strip__Orient__vert, $__wDebugCanvas);
+            var $__wDebugStatesExtender = new $Widget($__wDebugStates);
+            $__wDebugStates.$addItem($__wDebugStatesExtender, '64px');
+            var $__wDebugStatesStates = new $Widget($__wDebugStates);
+            $__wDebugStates.$addItem($__wDebugStatesStates);
+            $d.$__debug.$publisher.$registerSubscriber(new $DebugStatesWidget($__wDebugStates, $__wDebugStatesExtender, $__wDebugStatesStates));
+            $__wDebugCanvas.$addItem($__wDebugStates);
         }
-        $d.__debug.$publisher.registerSubscriber(new DebugConsole());
+        $d.$__debug.$publisher.$registerSubscriber(new $DebugConsole());
     }
     else
-        $d.$wRoot = new Widget();
-    if ($o.$states.hasOwnProperty('tree')){
-        $d.stateToUriMapper = new StateToUriMapper({
-            initPath: $o.$states.initPath,
+        $d.$wRoot = new $Widget();
+    if ($o.$states.hasOwnProperty('$tree')){
+        $d.$stateToUriMapper = new $StateToUriMapper({
+            $initPath: $o.$states.$initPath,
             $initState: $o.$states.$initState,
-            tree: $o.$states.tree,
-            stringifyState: $o.$states.stringifyState
+            $tree: $o.$states.$tree,
+            $stringifyState: $o.$states.stringifyState
         });
-        $d.$publisher.registerSubscriber($d.stateToUriMapper);
+        $d.$publisher.$registerSubscriber($d.$stateToUriMapper);
     }
     
-    $d.$wRoot._div.className = '$wRoot';//
-    $d.curLayout = undefined;
-    $d.$wRoot._onResized = function($d, f){return function($w,h){
-        var lay = $d.$o.get_layout($w,h,$d.$o.$target);
-        Widget.prototype._onResized.call($d.$wRoot, $w, h);
-        f(lay);
+    $d.$wRoot.$_div.className = '$wRoot';//
+    $d.$curLayout = undefined;
+    $d.$wRoot.$_onResized = function($d, $f){return function($w,$h){
+        var $lay = $d.$o.$get_layout($w,$h,$d.$o.$target);
+        $Widget.prototype.$_onResized.call($d.$wRoot, $w, $h);
+        $f($lay);
         for ($0 = 0 ; $0 < $d.$logics.length ; $0++){
-            $d.$logics[$0].initializeGui($d.$o.$target, lay);
+            $d.$logics[$0].$initializeGui($d.$o.$target, $lay);
         }
     };}($d, $switchToLayout);
 
-    for ($0 = 0 ; $0 < $o.$states.stateSubscribers.length ; $0++){
-        $1 = $o.$states.stateSubscribers[$0];
+    for ($0 = 0 ; $0 < $o.$states.$stateSubscribers.length ; $0++){
+        $1 = $o.$states.$stateSubscribers[$0];
+        #USAGE_BEGIN#debug##
         console.log('Создаётся экземпляр подписчика ' + $1.name);
+        #USAGE_END#debug##
         if (typeof $1 === 'function')
             $3 = new $1();
         else
-            $3 = (new $1[0]()).substate($1[1]);
+            $3 = (new $1[0]()).$substate($1[1]);
         $d.$logics.push($3);
-        $d.$publisher.registerSubscriber($3);
+        $d.$publisher.$registerSubscriber($3);
     }
-    if ($o.hasOwnProperty('extender')){
-        $d.extender = {
+    if ($o.hasOwnProperty('$extender')){
+        $d.$extender = {
             $o: undefined,
             $w: {}, // WidgetStateSubscriber
             $widgets: {}, // _WidgetBase
-            extender: undefined
+            $extender: undefined
         };
-        var $3 = new Догрузчик($o.extender.getUrl, $d.extender);
-        if ($o.extender.hasOwnProperty('substate'))
-            $3 = $3.useSubstate($o.extender.substate);
-        $d.$publisher.registerSubscriber($3);
+        var $3 = new $Догрузчик($o.$extender.$getUrl, $d.$extender);
+        if ($o.$extender.hasOwnProperty('$substate'))
+            $3 = $3.$useSubstate($o.$extender.$substate);
+        $d.$publisher.$registerSubscriber($3);
     }
 };
 '======================================';
