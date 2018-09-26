ed2429bdde9d47ed39266cb20c5886908a15005f
VasilyevBP <1024sparrow@gmail.com>
2018-09-26 13:46:41 +0300

    123

--- build_tests/ubuntu_desktop/main.cpp
+++ build_tests/ubuntu_desktop/main.cpp
@@ -1,17 +1,42 @@
 #include <QApplication>
 #include <QWebView>
 #include <QWebFrame>
+#include <QPixmap>
+#include <QSplashScreen>
+#include <QBitmap>
 
+#include "main.h"
 #include "api_native.h"
 
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
-
-    QWebView wv;
-    wv.page()->mainFrame()->addToJavaScriptWindowObject("TralivaApi", new ApiNative);
-    wv.load(QUrl("qrc:///web_content/index.html"));
-    wv.show();
-
+    Main main(argc, argv);
     return app.exec();
+}
+
+//static QPixmap pix(":/splash.png");
+
+Main::Main(int argc, char **argv)
+    :QObject(0)
+{
+    QPixmap pix(":/splash.png");
+    splashScreen = new QSplashScreen(pix);
+    //QSplashScreen splash(pix);
+    //pix.mask().save("/home/user/1.png");
+    splashScreen->setMask(pix.mask());
+    //splashScreen->showMessage("hello!", Qt::AlignCenter, Qt::yellow);
+    splashScreen->show();
+    //app.processEvents();
+
+    wv = new QWebView();
+    wv->page()->mainFrame()->addToJavaScriptWindowObject("TralivaApi", new ApiNative);
+    connect(wv, SIGNAL(loadFinished(bool)), this, SLOT(onLoadFinished(bool)));
+    wv->load(QUrl("qrc:///web_content/index.html"));
+}
+
+void Main::onLoadFinished(bool p_ok)
+{
+    splashScreen->finish(wv);
+    wv->show();
 }
--- build_tests/ubuntu_desktop/qt4webkit.pro
+++ build_tests/ubuntu_desktop/qt4webkit.pro
@@ -4,7 +4,10 @@ SOURCES += \
     main.cpp \
     api_native.cpp
 
-RESOURCES += content.qrc
+RESOURCES += \
+    content.qrc \
+    splash_and_icons.qrc
 
 HEADERS += \
-    api_native.h
+    api_native.h \
+    main.h
