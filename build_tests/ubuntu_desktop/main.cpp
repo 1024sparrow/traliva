14c36886063ef10cb2506a2613306ada0bfbd11d
VasilyevBP <1024sparrow@gmail.com>
2018-09-26 14:03:44 +0300

    test app for ubuntu_desktop ready

--- build_tests/ubuntu_desktop/main.cpp
+++ build_tests/ubuntu_desktop/main.cpp
@@ -8,28 +8,26 @@
 #include "main.h"
 #include "api_native.h"
 
+const QString M_APPNAME = "asv";
+
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
+    app.setWindowIcon(QIcon(":/icon.png"));
     Main main(argc, argv);
     return app.exec();
 }
-
-//static QPixmap pix(":/splash.png");
 
 Main::Main(int argc, char **argv)
     :QObject(0)
 {
     QPixmap pix(":/splash.png");
     splashScreen = new QSplashScreen(pix);
-    //QSplashScreen splash(pix);
-    //pix.mask().save("/home/user/1.png");
     splashScreen->setMask(pix.mask());
-    //splashScreen->showMessage("hello!", Qt::AlignCenter, Qt::yellow);
     splashScreen->show();
-    //app.processEvents();
 
     wv = new QWebView();
+    wv->setWindowTitle(M_APPNAME);
     wv->page()->mainFrame()->addToJavaScriptWindowObject("TralivaApi", new ApiNative);
     connect(wv, SIGNAL(loadFinished(bool)), this, SLOT(onLoadFinished(bool)));
     wv->load(QUrl("qrc:///web_content/index.html"));
