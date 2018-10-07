6d7e3b74ab894ba1945d9c204223bf58a7d358eb
1024sparrow <1024sparrow@gmail.com>
2018-10-07 13:27:21 +0300

    123

--- src/build_scripts/traliva_final.sh
+++ src/build_scripts/traliva_final.sh
@@ -7,10 +7,24 @@
 . $1/src/config
 
 compiled_dir="$1/$2/targets"
+targets_dir="$1/targets"
 mkdir "$compiled_dir"
+mkdir "$targets_dir"
 for i in $(ls -1 $1/src/build_scripts/targets)
 do
     echo "Запускается скрипт генерации исходного кода под платформу \"$i\""
-    mkdir "$compiled_dir"/"$i"
+    mkdir "$targets_dir"/"$i"
+
+    pushd "$targets_dir"/"$i"
+    if [ ! -d "$targets_dir"/"$i"/.git ]
+    then
+            git init
+    fi
+    popd
+
+    pushd "$compiled_dir"
+        git clone "$1"/targets/"$i"/.git
+    popd
+    #mkdir "$compiled_dir"/"$i"
     $1/src/build_scripts/targets/"$i"/init.sh "$1/$2/project" "$compiled_dir"/"$i"
 done
