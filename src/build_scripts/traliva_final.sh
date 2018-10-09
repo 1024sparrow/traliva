ddf03b098c4d105285275a9839afed4d7c6928bf
1024sparrow <1024sparrow@gmail.com>
2018-10-09 05:25:48 +0300

    123

--- src/build_scripts/traliva_final.sh
+++ src/build_scripts/traliva_final.sh
@@ -24,22 +24,18 @@ do
 
     pushd "$compiled_dir"
         git clone "$1"/targets/"$i"/.git
-        git checkout -b skeleton
-        #git pull
-        rm -r * # скрытые файлы и директории, в том чисел и .git, никуда не деваются
-    popd
-
-    #mkdir "$compiled_dir"/"$i"
-    $1/src/build_scripts/targets/"$i"/init.sh "$1/$2/project" "$compiled_dir"/"$i"
-
-    pushd "$compiled_dir"
-        # if [есть локальные изменения]; then
-        if ! git diff-index --quiet HEAD --
-        then
-            git add .
-            git commit -m"123"
-            #git push --set-upstream origin skeleton
-        fi
+        pushd "$compiled_dir"/"$i"
+            git checkout -b skeleton
+            git pull
+            rm -r * # скрытые файлы и директории, в том чисел и .git, никуда не деваются
+            $1/src/build_scripts/targets/"$i"/init.sh "$1/$2/project" "$compiled_dir"/"$i"
+            if ! git diff-index --quiet HEAD --
+            then
+                git add .
+                git commit -m"traliva: skeleton changed for target \"$i\""
+                git push --set-upstream origin skeleton
+            fi
+        popd
     popd
 
     pushd "$targets_dir"/"$i"
