7380af6fe1d52a53e4d223444f6150f2536d19eb
1024sparrow <1024sparrow@gmail.com>
2018-11-11 07:35:29 +0300

    123

--- traliva/src/body/init/state_to_url_mapper.js
+++ traliva/src/body/init/state_to_url_mapper.js
@@ -25,7 +25,7 @@ function $StateToUrlMapper($p_statesObj){
     this.$prevAr = [];
     this.$isVirgin = true;
     console.log('%%% initPath:', this.$initPath);
-}
+};
 $StateToUrlMapper.prototype = Object.create($StateSubscriber.prototype);
 $StateToUrlMapper.prototype.constructor = $StateToUrlMapper;
 /*
@@ -54,13 +54,14 @@ $StateToUrlMapper.prototype.$processStateChanges = function(s){
     }
     //$Traliva.$history.pushState('/123/123/123');
     //pushState('asd/asd/asd');
-}
+};
 $StateToUrlMapper.prototype.$updateForUrl = function($p_url, $p_ifInit){
     console.log('--', $p_url, '-- initPath:', this.$initPath);
     //this.$_tree, this.$initPath, this.$initPathLength
     var url = $p_url.slice(this.$initPathLength);
     var ar = [];
-    var i, cand;
+    var stateChanged = false;
+    var i, cand, o;
     for (i = url.indexOf('/', 1) ; i >= 0 ; i = url.indexOf('/', 1)){
         cand = url.slice(1, i);
         if (cand.length) // встречающиеся двойные слеши трактуем как одинарные
@@ -69,10 +70,50 @@ $StateToUrlMapper.prototype.$updateForUrl = function($p_url, $p_ifInit){
         console.log('*', i, ar, url);
     }
     console.log('*-*', i, ar, url);
+    var b = 0, bTree = this.$_tree, stack;
     if ($p_ifInit){
         this.$_state = $Traliva.$__d.$o.$states.$initState;
     }
-}
+    else{
+        stack = this.$_tree.slice();
+        for (i = 0 ; i < ar.length ; ++i){
+            if (i >= this.$prevAr.length)
+                break;
+            b = i;
+            if (ar[i] !== this.$_prevAr[i]){
+                break;
+            }
+            //bTree = (i === 0) ? bTree[ar[i]] : bTree.$d[ar[i]];
+            if (i === 0)
+                bTree = bTree[ar[i]];
+            else{
+                bTree = bTree.$d[ar[i]];
+                if (bTree.params){
+                    // boris e: проверить, насколько параметры не изменились: если изменились, то записать изменения в соответствующие подсостояния
+                    stateChanged = true;
+                }
+            }
+        }
+        console.log('b: ' + b + ', bTree:', bTree);
+    }
+    ++b;
+    o = bTree;
+    // Производим деструкцию старых элементов от b и bTree
+    for (i = b ; i < this.$prevAr.length ; ++i){
+        o = (i === 0) ? o[this.$prevAr[i]] : o.$d[this.$prevAr[i]];
+        if (i)
+            o = 
+        // если узел дерева имеет параметры, то соответствующие подсостояния не трогаем - только по substate и name
+        if (o.$name)
+            this.$setSubstate(o.$substate, '');
+        else
+            this.$setSubstate(o.$substate, undefined);
+    }
+
+    // Производим конструкцию новых элементов от b и bTree
+
+    this.$_registerStateChanges();
+};
 // в соответствии с текущим URL устанавливаем нужные значения в state
 /*$StateToUrlMapper.prototype.$updateState = function(){
     console.log('---');
@@ -85,3 +126,7 @@ $StateToUrlMapper.prototype.$updateForUrl = function($p_url, $p_ifInit){
     }
     this.$_registerStateChanges();
 }*/
+$StateToUrolMapper.prototype.$setSubstate = function(){
+};
+$StateToUrlMapper.prototype.$getSubstate = function(){
+};
