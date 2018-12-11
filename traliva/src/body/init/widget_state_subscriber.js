c4c594957234542a1f51370fefdaf83a8a44b822
1024sparrow <1024sparrow@gmail.com>
2018-12-12 01:03:44 +0300

    123

--- traliva/src/body/init/widget_state_subscriber.js
+++ traliva/src/body/init/widget_state_subscriber.js
@@ -18,15 +18,33 @@ Array.prototype.set = function($0, $1){
     else{
         this[$0] = $1;
     }
+    return this;
 };
 $WidgetStateSubscriber__makeArrayReportable = function($p_reportTaker, $p_arr, $p_id){
     if ($p_arr === undefined){
         // boris here
     }
     else{
-        $p_arr.set = function($0, $1){
-            if ($0 instanceof Array){
+        $p_arr.set = function($p_0, $p_1){
+            var $tmp, $cand, $w, $0, $1, $2;
+            if ($p_0 instanceof Array){
                 // boris here
+                // убираем старых детей
+                for ($1 = $p_0.length ; $1 < $p_reportTaker.$__WidgetStateSubscriber.$childrenWidgets[$p_id].length ; ++$1){
+                    console.log('-- removing child WSS --');
+                    $tmp = $p_reportTaker.$__WidgetStateSubscriber.$childrenWidgets[$1];
+                    $tmp.$destroy();
+                    $Traliva.$__d.$publisher.$unregisterSubscriber($tmp);
+                }
+                // добавляем новых детей
+                for ($1 = $p_reportTaker.$__WidgetStateSubscriber.$childrenWidgets[$p_id].length ; $1 <= $p_0.length ; ++$1){
+                    console.log('-- adding child WSS --');
+                    $tmp = $p_reportTaker.$__WidgetStateSubscriber.$descr.$children[$p_id];
+                    $w = new $Widget($p_reportTaker.$__WidgetStateSubscriber.$wContainer, undefined);// кстати, про второй параметр p_attr ...
+                    $cand = new $tmp.$constructor($w, $tmp.$options, $tmp);
+                    $p_reportTaker.$__WidgetStateSubscriber.$childrenWidgets[$p_id].push($cand);
+                }
+                $p_reportTaker.$__WidgetStateSubscriber.$childrenChanged[$p_id] = 1;
             }
             else{
                 // boris here
@@ -56,6 +74,7 @@ function $WidgetStateSubscriber($p_wContainer, $p_options, $p_descr){
         $wContainer: $p_wContainer,
         $descr: $p_descr,
         $children: {}, // подсостояния-массивы
+        $childrenChanged: {}, // set: все значения - 1
         $childrenWidgets: {} // массивы дочерних виджетов
     };
     if ($p_options && $p_options.hasOwnProperty('$bg'))
@@ -93,6 +112,7 @@ $WidgetStateSubscriber.prototype.$processStateChanges = function(s){
             }
         }
     }
+    console.log('WSS::processStateChanges: ', this);//
 };
-$WidgetStateSubscriber.prototype.$destroy = function(){};//уничтожить созданный ранее DOM-элемент
+$WidgetStateSubscriber.prototype.$destroy = function(){};//уничтожить созданный ранее DOM-элемент, вызывается перед отписыванием от издателя
 //$WidgetStateSubscriber.prototype.$_increaseChildrenSize
