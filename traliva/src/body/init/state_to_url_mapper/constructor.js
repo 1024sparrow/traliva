838c6f95228c4c936056318bdc0f19b69b8114d5
1024sparrow <1024sparrow@gmail.com>
2019-02-08 05:12:48 +0300

    123

---
+++ traliva/src/body/init/state_to_url_mapper/constructor.js
@@ -0,0 +1,91 @@
+// если $o.$tree не существует, то экземпляр и не создаётся
+function $StateToUrlMapper($p_statesObj){
+    $Traliva.$StateSubscriber.call(this);
+    this.$_statesObj = $p_statesObj;
+    this.$_ajax = new $Traliva.$Ajax();
+    var $uri = window.location.href,
+        $0, $tmp, $stack,
+        $1,
+        $2 = 2,//два слэша
+        $3
+    ;
+    $1 = $uri.indexOf('//');
+    if ($uri[0] == '/'){
+        $1++;
+        $2++;
+    }
+    $3 = $uri.indexOf('/', $1 + $2) + 1;// '/' тоже является частью URL
+    //$3 += $p_statesObj.initPath.length;//<--
+    this.$used = 0;
+    this.$initPath = $uri.substr(0, $3);
+    console.log('INIT PATH: ', this.$initPath);//
+    //this.$initPathLength = $3 + 1; 
+    this.$initPathLength = $3 - 1; 
+    this.$_tree = $Traliva.$__d.$o.$states.$tree;
+    this.$_debugMode = $Traliva.$debug && $Traliva.$debug.$url;
+    this.$urlUpdating = false;
+    if ((this.$initPath.substr(0, 7) === 'file://') && !($Traliva.$debug && $Traliva.$debug.$url)){
+        console.error('Запуск маппера URL в состояние из файловой системы возможен только при активированном режиме отладки \'url\'');
+    }
+    if (this.$_debugMode){
+        this.$initPath = 'http://' + $Traliva.$debug.$url; // без '/' на конце
+        this.$initPathLength = this.$initPath.length;
+    }
+    this.$prevAr = [];//
+    this.$isVirgin = true;
+    //console.log('%%% initPath:', this.$initPath);
+
+    // в массивах (корневой и d-свойства (дети)) - свойство __list: список предков, начиная от самого себя
+    $stack = [this.$_tree];
+    this.$_tree.__list = [this.$_tree];
+    while ($stack.length){
+        $0 = $stack.shift();
+        for ($1 = 0 ; $1 < $0.length ; ++$1){
+            for ($2 in $0[$1]){
+                //console.log('+', $0[$1][$2]);//----
+                if ($0[$1][$2].$d){
+                    $3 = $0[$1][$2].$d;
+                    $3.__list = $0.__list.slice();
+                    $3.__list.unshift($3);
+                    $stack.unshift($3);
+                }
+            }
+        }
+    }
+    /*$stack = [this.$_tree];
+    $tmp = [this.$_tree];
+    // наполняем $tmp
+    var counter = 0;//
+    while ($stack.length){
+        console.log('tick ', counter++);//
+        if (counter > 200){
+            console.error('FATAL');
+            return;
+        }
+        $0 = $stack.shift();
+        for ($1 = 0 ; $1 < $0.length ; ++$1){
+            for ($2 in $0[$1]){
+                if ($0[$1][$2].$d){
+                    $3 = $0[$1][$2].$d;
+                    $tmp.push($3);
+                    $stack.unshift($3);
+                    //$stack.push($3);//
+                }
+            }
+        }
+    }
+    console.log('TMP: ', $tmp.slice());//----
+    // второй проход по стеку тем же маршрутом с постепенно тающим содержимым $tmp
+    while ($tmp.length){
+        //$1 = $tmp.slice();
+        $2 = $tmp.shift();
+        $1 = $tmp.slice();//
+        $2.__list = $1;
+    }
+    console.log('TREE: ', this.$_tree);//----
+    //return;//
+    */
+
+
+    console.log('StateToUrlMapper: debugMode ' + (this.$_debugMode ? 'enabled' : 'disabled'));
+};
---
+++ traliva/src/body/init/state_to_url_mapper/process_state_changes.js
@@ -0,0 +1,69 @@
+$StateToUrlMapper.prototype.$processStateChanges = function(s){
+    console.group('processStateChanges');
+    if (this.$isVirgin){
+        this.$isVirgin = false;
+        if (this.$_debugMode){
+            this.$updateForUrl(this.$initPath, true);
+        }
+        else{
+            window.onpopstate = (function($0){
+                return function(){$0(document.location.href);};
+            })(this.$updateForUrl);
+            this.$updateForUrl(window.location.href, true);
+        }
+    }
+    // обойти дерево и составить url. Если совпадает, то ничего не делаем. Если не совпадает - просто заменяем текущий URL.
+    var $0, $1, $2, $3, $4, $5;
+    var $cand = this.$initPath;
+    if ($Traliva.$debug && $Traliva.$debug.$url)
+        $cand += '/';
+    console.log('init path: ', $cand);
+    var $stack = this.$_tree.slice();
+    //console.log('**', this.$_tree);//
+    while ($stack.length){
+        $0 = $stack.shift();
+        console.log('----', $0);
+        $for_2: for ($2 in $0){
+            $1 = $0[$2];
+            //console.log('debugName: ', $1.$debugName);//
+            console.log('state tree item name: ', $2);//
+            $3 = this.$getSubstate($1.$substate);
+            //console.log('$3 = ' + $3);
+            if ($1.hasOwnProperty('$name'))
+                $3 = $3 === $1.$name;
+            if ($3 && $1.$params){
+                $3 = '';
+                console.log('params: ', $1.$params);//
+                for ($4 = 0 ; $4 < $1.$params.length ; ++$4){
+                    $5 = this.$getSubstate($1.$params[$4]);
+                    if ($5 === undefined){
+                        console.log('ERROR: подсостояние параметра не доступно: ' + $1.$params[$4]);
+                        break $for_2;
+                    }
+                    $3.push($5.toString() + '/');
+                }
+            }
+            if ($3){
+                $cand += $2 + '/';
+                if ($1.$params){
+                    $cand += $3;
+                }
+                if ($1.$d){
+                    for ($4 = $1.$d.length - 1 ; $4 >= 0 ; --$4){
+                        $stack.unshift($1.$d[$4]);
+                    }
+                }
+                break $for_2;
+            }
+        }
+    }
+    //console.log('test URL: ', $cand);
+    $0 = $Traliva.$history.$_current();
+    //console.log('current URL:', $Traliva.$history.$_current());
+    if ($cand !== $0){
+        this.$urlUpdating = true;
+        $Traliva.$history.replaceState({}, '', $cand);
+        this.$urlUpdating = false;
+    }
+    console.groupEnd();
+};
