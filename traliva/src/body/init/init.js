daba513893e92d16f1fa3c72a3b5f56920412481
1024sparrow <1024sparrow@gmail.com>
2019-03-10 04:37:11 +0300

    build fixed

--- traliva/src/body/init/init.js
+++ traliva/src/body/init/init.js
@@ -46,20 +46,22 @@ $Traliva.$init = function($o){
     //$1 - tmp
     //$3 - cand
 
-    $1 = document.createElement('div');
-    $1.style.height = '1in';
-    $1.style.width = '1in';
-    document.body.appendChild($1);
-    $0 = $1.clientHeight;
-    $3 = $1.clientWidth;
-    $Traliva.$pixelPerInch = ($0 && $3) ? {
-        $hor: $3,
-        $vert: $0
-    } : {
-        $hor: 96,
-        $vert: 96
-    };
-    document.body.removeChild($1);
+    if (document){ // этот скрипт, помимо того как из браузера, вызывается также и из генератора серверного кода на NodeJS, а там нет document.
+        $1 = document.createElement('div');
+        $1.style.height = '1in';
+        $1.style.width = '1in';
+        document.body.appendChild($1);
+        $0 = $1.clientHeight;
+        $3 = $1.clientWidth;
+        $Traliva.$pixelPerInch = ($0 && $3) ? {
+            $hor: $3,
+            $vert: $0
+        } : {
+            $hor: 96,
+            $vert: 96
+        };
+        document.body.removeChild($1);
+    }
 
     #USAGE_BEGIN#debug##
     console.log('начинаю инициализацию');
