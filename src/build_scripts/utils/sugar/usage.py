b072619d82d6553450f78f447e72ea00df285140
1024sparrow <1024sparrow@gmail.com>
2019-03-27 01:41:29 +0300

    123

--- src/build_scripts/utils/sugar/usage.py
+++ src/build_scripts/utils/sugar/usage.py
@@ -22,7 +22,6 @@ def process(p_js, p_css, p_js_css):
         #    continue#
         for fragment in fil['text']:
             if fragment['type'] == 1:
-                #print('#### INIT:\n', fragment['text'])
                 s = 0
                 a = '' # изменённый текст на замещение старого
                 id_cand = ''
@@ -113,6 +112,7 @@ def process(p_js, p_css, p_js_css):
                 fragment['text'] = a
     #print('detected activated: ', activated_ids)
     #print('detected clusters: ', clusters)
+
     additional_activated_ids = []
     for i in activated_ids:
         if i in clusters:
@@ -120,112 +120,103 @@ def process(p_js, p_css, p_js_css):
                 additional_activated_ids.append(ii)
     for i in additional_activated_ids:
         activated_ids.add(i)
-
-    id_current = None
-    activated_ids__0 = ['#USAGE_DEGIN#%s##' % i for i in activated_ids]
-    activated_ids__1 = ['#USAGE_END#%s##' % i for i in activated_ids]
-    #print(activated_ids__0)
-    f_begin = lambda p:{
-        #index_fragm:
-    }
+    # применяем активаторы
     for fil in p_js_css:
-        m = []
-        fragment_counter = 0
+        cur_blocks = []
+        ifEnable = True
+        #id_cand = ''
         for fragment in fil['text']:
-            if fragment['type'] == 1:
-                #[m.start() for m in re.finditer('test', 'test test test test')]
-                # a = 's f#USAGE_BEGIN#qwed3##ddf 4r'
-                # [(i.start(), i.group()) for i in re.finditer('#USAGE_BEGIN#(.*)##', a)]
-                for patt in [(0,'#USAGE_BEGIN#(.*)##'), (1,'#USAGE_END#(.*)##')]:
-                    for i in re.finditer(patt[1], fragment['text']):
-                        m.append({
-                            'fragment_index': fragment_counter,
-                            'char_index': i.start(),
-                            'id': i.groups()[0],
-                            'string': i.group(),
-                            'type': patt[0],
-                            #'order': order
-                        })
-            fragment_counter += 1
-        m = sorted(m, key=lambda p: p['fragment_index'] * 0x100000000000000000000000000000 + p['char_index'])
+            s = 0
+            cand = ''
+            broken = ''
+            for i in fragment['text']:
+                if fragment['type'] == 1:
+                    if s == 0 and i == '#':
+                        s = 1
+                    elif s == 1 and i == 'U':
+                        s = 2
+                    elif s == 2 and i == 'S':
+                        s = 3
+                    elif s == 3 and i == 'A':
+                        s = 4
+                    elif s == 4 and i == 'G':
+                        s = 5
+                    elif s == 5 and i == 'E':
+                        s = 6
+                    elif s == 6 and i == '_':
+                        s = 7
+                    elif s == 7 and i == 'B':
+                        s = 8
+                    elif s == 8 and i == 'E':
+                        s = 9
+                    elif s == 9 and i == 'G':
+                        s = 10
+                    elif s == 10 and i == 'I':
+                        s = 11
+                    elif s == 11 and i == 'N':
+                        s = 12
+                    elif s == 12 and i == '#':
+                        s = 13
+                        id_cand = ''
+                    elif s == 13 and is_letterdigit(i):
+                        #s = 14
+                        id_cand += i
+                    elif s == 13 and i == '#':
+                        s = 15
+                        cur_blocks.append(id_cand)
+                        ifEnable = _updateEnableState(cur_blocks, activated_ids)
+                    elif s == 15 and i == '#':
+                        s = 16
+                    elif s == 16:
+                        s = 0
+                        # ...
+                    elif s == 7 and i == 'E':
+                        s = 108
+                    elif s == 108 and i == 'N':
+                        s = 109
+                    elif s == 109 and i == 'D':
+                        s = 110
+                    elif s == 110 and i == '#':
+                        s = 113
+                        id_cand = ''
+                    elif s == 113 and is_letterdigit(i):
+                        #s = 114
+                        id_cand += i
+                    elif s == 113 and i == '#':
+                        s = 115
+                        if id_cand != cur_blocks[-1]:
+                            print('SYNTAX ERROR!!')
+                        cur_blocks = cur_blocks[:-1]
+                        ifEnable = _updateEnableState(cur_blocks, activated_ids)
+                    elif s == 115 and i == '#':
+                        s = 116
+                    elif s == 116:
+                        s = 0
+                        # ...
+                    else:
+                        if s == 15:
+                            print('ERROR')
+                        elif s == 115:
+                            print('ERROR')
+                        elif s != 0:
+                            cand += broken
+                            s = 0
+                    #if s == 0:
+                    #    cand += broken
+                    #    broken = ''
+                    if s == 1:
+                        broken = i
+                    elif s == 0 or s == 16 or s == 116:
+                        broken = ''
+                    else:
+                        broken += i
+                if s == 0 and ifEnable:
+                    cand += i
+            cand += broken
+            fragment['text'] = cand
 
-        #mm = []
-        #mm_max_0 = 0
-        #mm_max_1 = 0
-        #for i in m:
-        #    print('---', i)
-        #    if i['fragment_index'] >= mm_max_0
-
-        #for i in range(0, len(m)):
-        i = 0
-        prev_paragr = None
-        prev_file = None
-        while i < len(m)//2:
-            i_0 = 2 * i
-            i_1 = i_0 + 1
-            m_0 = m[i_0]
-            m_1 = m[i_1]
-            _process(m_0, m_1, fil['text'], activated_ids, m)
-            i += 1
-
-        # файл уже отсортирован
-        #while m:
-        #    print('-#-')
-        #    print(m)
-        #    # ищем пару откр-закр с одним id, так чтобы они шли один за другим сразу.
-        #    counter = 0
-        #    prev = None
-        #    found = None
-        #    for i in m:
-        #        if counter:
-        #            if prev['id'] == i['id'] and prev['type'] == 0 and i['type'] == 1:
-        #                _process(prev, i, fil['text'], activated_ids)
-        #                found = counter
-        #                #break
-        #        counter += 1
-        #        prev = i
-        #    if counter:
-        #        # должны удалить из m элементы с индексами counter - 1 и counter
-        #        m = m[:counter - 1] + m[counter + 1:]
-        #    else:
-        #        print('Некорректная разметка #USAGE...#..##: у вас либо какой-то блок не закрывается, либо блоки перекрываются (что недопустимо).')
-        #        exit(1)
-
-# возвращает количество удалённых символов
-def _process(p1, p2, p3, p4, p_fullmap):
-    retval = 0
-    id = p1['id']
-    start_char_index = p1['char_index'] 
-    start_char_endindex = start_char_index + len(p1['string']) 
-    end_char_index = p2['char_index'] 
-    end_char_endindex = end_char_index + len(p2['string']) 
-
-    # удаляем из текста начало блока и, если надо, весь текст внутри блока
-    if id in p4: # тэг активирован, убираем только разметку
-        p3[p1['fragment_index']]['text'] = p3[p1['fragment_index']]['text'][:start_char_index] + p3[p1['fragment_index']]['text'][start_char_endindex:]
-        p3[p2['fragment_index']]['text'] = p3[p2['fragment_index']]['text'][:end_char_index] + p3[p2['fragment_index']]['text'][end_char_endindex:]
-        if p1['fragment_index'] == p2['fragment_index']:
-            retval = start_char_endindex - start_char_index
-        retval += end_char_endindex - end_char_index
-    else: # тэг не активирован - убираем как саму разметку, так и текст внутри разметки
-        if p1['fragment_index'] == p2['fragment_index']:
-            p3[p1['fragment_index']]['text'] = p3[p1['fragment_index']]['text'][:start_char_index] + p3[p2['fragment_index']]['text'][end_char_endindex:]
-            retval = end_char_endindex - start_char_index
-        else:
-            p3[p1['fragment_index']]['text'] = p3[p1['fragment_index']]['text'][:start_char_index]
-            p3[p2['fragment_index']]['text'] = p3[p2['fragment_index']]['text'][end_char_endindex:]
-            retval = end_char_endindex
-
-    # удаляем, если надо, содержимое всех блоков, которые оказались между теми двумя блоками
-    if not id in p4:
-        for i in range(p1['fragment_index'] + 1, p2['fragment_index']):
-            p3[i]['text'] = ''
-
-    # корректируем индексы символов далее в параграфе
-    #print('uiytiuytiuytiuytiuyt:',p3)
-    if retval > 0:
-        for i in p_fullmap:
-            if p2['fragment_index'] == i['fragment_index']:
-                i['char_index'] = i['char_index'] - retval
-            #print('88888:',i)
-    return retval
+def _updateEnableState(p_curBlocks, p_activatedSet):
+    for i in p_curBlocks:
+        if not i in p_activatedSet:
+            return False
+    return True
