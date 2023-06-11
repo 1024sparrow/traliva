b3cc6778bd0d871717c59b0db972260a53b53ac8
Boris Vasilyev <1024sparrow@gmail.com>
2023-06-11 19:41:15 +0300

    develop-v2.2: traliva_final.sh: retab

--- src/build_scripts/traliva_final.sh
+++ src/build_scripts/traliva_final.sh
@@ -4,7 +4,7 @@
 #echo $1 -- /home/boris/da/pro/traliva
 #echo $2 -- compiled
 
-. $1/src/config
+source $1/src/config
 
 compiled_dir="$1/$2/targets"
 targets_dir="$1/targets"
@@ -12,43 +12,43 @@ mkdir "$compiled_dir"
 mkdir "$targets_dir"
 for i in $(ls -1 $1/src/build_scripts/targets)
 do
-    if [ ! -d "$1/src/build_scripts/targets/$i" ]
-    then
-        continue
-    fi
-    if [[ "$i" == _* ]]
-    then
-        continue
-    fi
-    echo "Запускается скрипт генерации исходного кода под платформу \"$i\""
-    mkdir "$targets_dir"/"$i"
+	if [ ! -d "$1/src/build_scripts/targets/$i" ]
+	then
+		continue
+	fi
+	if [[ "$i" == _* ]]
+	then
+		continue
+	fi
+	echo "Запускается скрипт генерации исходного кода под платформу \"$i\""
+	mkdir "$targets_dir"/"$i"
 
-    pushd "$targets_dir"/"$i"
-        if [ ! -d .git ]
-        then
-            git init
-        fi
-    popd
+	pushd "$targets_dir"/"$i"
+		if [ ! -d .git ]
+		then
+			git init
+		fi
+	popd
 
-    pushd "$compiled_dir"
-        git clone "$1"/targets/"$i"/.git
-        pushd "$compiled_dir"/"$i"
-            git checkout -b skeleton
-            git pull # если `git remote -v` не пустой
-            rm -r * # скрытые файлы и директории, в том чисел и .git, никуда не деваются
-            $1/src/build_scripts/targets/"$i"/init.sh "$1/$2/project" "$compiled_dir"/"$i"
-            if ! git diff-index --quiet HEAD --
-            then
-                git add .
-                git commit -m"traliva: skeleton changed for target \"$i\""
-                git push --set-upstream origin skeleton
-            fi
-        popd
-    popd
+	pushd "$compiled_dir"
+		git clone "$1"/targets/"$i"/.git
+		pushd "$compiled_dir"/"$i"
+			git checkout -b skeleton
+			git pull # если `git remote -v` не пустой
+			rm -r * # скрытые файлы и директории, в том чисел и .git, никуда не деваются
+			$1/src/build_scripts/targets/"$i"/init.sh "$1/$2/project" "$compiled_dir"/"$i"
+			if ! git diff-index --quiet HEAD --
+			then
+				git add .
+				git commit -m"traliva: skeleton changed for target \"$i\""
+				git push --set-upstream origin skeleton
+			fi
+		popd
+	popd
 
-    pushd "$targets_dir"/"$i"
-        git stash
-        git merge skeleton
-        git stash pop
-    popd
+	pushd "$targets_dir"/"$i"
+		git stash
+		git merge skeleton
+		git stash pop
+	popd
 done
