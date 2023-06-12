47821128ba2de2ec5218aeb4dd90012d658dac6a
Boris Vasilyev <1024sparrow@gmail.com>
2023-06-12 23:14:13 +0300

    develop-v2.2

--- src/build_scripts/traliva_final.sh
+++ src/build_scripts/traliva_final.sh
@@ -6,76 +6,61 @@
 
 source $1/src/config
 
-compiled_dir="$1/$2/targets"
-targets_dir="$1/targets"
+readonly compiled_dir="$1/$2/targets"
+readonly targets_dir="$1/targets"
+declare compiled
+declare compiledPrev
+declare init
+
 mkdir "$compiled_dir"
 mkdir "$targets_dir"
 declare tempFile
+if ! [ -d "$1"/compiledPrev ]
+then
+	mkdir "$1"/compiledPrev
+fi
 for i in $(ls -1 $1/src/build_scripts/targets)
 do
 	if [ ! -d "$1/src/build_scripts/targets/$i" ]
 	then
 		continue
 	fi
-	if [[ "$i" == _* ]]
+	if [[ "$i" =~ ^_ ]]
 	then
 		continue
 	fi
-	echo "Запускается скрипт генерации исходного кода под платформу \"$i\""
 	if ! [ -d "$targets_dir"/"$i" ]
 	then
 		continue
 	fi
+	echo "Запускается скрипт генерации исходного кода под платформу \"$i\""
 
-	#pushd "$targets_dir"/"$i"
-	#	if [ ! -d .git ]
-	#	then
-	#		git init
-	#	fi
-	#popd
+	compiled="$compiled_dir"/"$i"
+	compiledPrev="$1"/compiledPrev/targets/"$i"
+	init=false
 
-	if ! [ -d "$compiled_dir"/"$i" ]
+	if ! [ -d "$compiledPrev" ]
 	then
-		mkdir "$compiled_dir"/"$i"
-		pushd "$compiled_dir"/"$i"
+		init=true
+		mkdir -p $compiledPrev
+		pushd $compiledPrev
 			git init -b skeleton
 		popd
 	fi
-
-	pushd "$compiled_dir"/"$i"
-		rm -r * # скрытые файлы и директории, в том чисел и .git, никуда не деваются
-		$1/src/build_scripts/targets/"$i"/init.sh "$1/$2/project" "$compiled_dir"/"$i"
-		tempFile=$(mktemp)
-		git diff > $tempFile
-		pushd "$targets_dir"/"$i"
-			git apply $tempFile
-		popd
-		if [ -n "$(cat $tempFile)" ]
+	$1/src/build_scripts/targets/"$i"/init.sh "$1/$2/project" "$compiled"
+	pushd "$compiledPrev"
+		rm -rf *
+		cp -r "$compiled"/* ./
+		if ! $init
 		then
-			git add . && git commit -m "traliva: skeleton changed for target \"$i\""
+			tempFile=$(mktemp)
+			git diff > $tempFile
+			cat $tempFile > /home/boris/da/tmp/230611
+			pushd "$targets_dir"/"$i"
+				git apply $tempFile
+			popd
+			rm $tempFile
 		fi
-		rm $tempFile
+		git add . && git commit -m "traliva: skeleton changed for target \"$i\""
 	popd
-
-	#pushd "$compiled_dir"
-	#	git clone "$1"/targets/"$i"/.git
-	#	pushd "$compiled_dir"/"$i"
-	#		git checkout -b skeleton
-	#		git pull # если `git remote -v` не пустой
-	#		rm -r * # скрытые файлы и директории, в том чисел и .git, никуда не деваются
-	#		$1/src/build_scripts/targets/"$i"/init.sh "$1/$2/project" "$compiled_dir"/"$i"
-	#		if ! git diff-index --quiet HEAD --
-	#		then
-	#			git add .
-	#			git commit -m"traliva: skeleton changed for target \"$i\""
-	#			git push --set-upstream origin skeleton
-	#		fi
-	#	popd
-	#popd
-
-	#pushd "$targets_dir"/"$i"
-	#	git stash
-	#	git merge skeleton
-	#	git stash pop
-	#popd
 done
