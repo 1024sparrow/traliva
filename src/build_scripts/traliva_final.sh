facd37b8b20cf04ac37bd2f55a49ed08376a5a9c
Boris Vasilyev <1024sparrow@gmail.com>
2023-06-12 03:01:35 +0300

    develop-v2.2

--- src/build_scripts/traliva_final.sh
+++ src/build_scripts/traliva_final.sh
@@ -10,6 +10,7 @@ compiled_dir="$1/$2/targets"
 targets_dir="$1/targets"
 mkdir "$compiled_dir"
 mkdir "$targets_dir"
+declare tempFile
 for i in $(ls -1 $1/src/build_scripts/targets)
 do
 	if [ ! -d "$1/src/build_scripts/targets/$i" ]
@@ -30,21 +31,32 @@ do
 		fi
 	popd
 
-	pushd "$compiled_dir"
-		git clone "$1"/targets/"$i"/.git
-		pushd "$compiled_dir"/"$i"
-			git checkout -b skeleton
-			git pull # если `git remote -v` не пустой
-			rm -r * # скрытые файлы и директории, в том чисел и .git, никуда не деваются
-			$1/src/build_scripts/targets/"$i"/init.sh "$1/$2/project" "$compiled_dir"/"$i"
-			if ! git diff-index --quiet HEAD --
-			then
-				git add .
-				git commit -m"traliva: skeleton changed for target \"$i\""
-				git push --set-upstream origin skeleton
-			fi
+	pushd "$compiled_dir"/"$i"
+		rm -r * # скрытые файлы и директории, в том чисел и .git, никуда не деваются
+		$1/src/build_scripts/targets/"$i"/init.sh "$1/$2/project" "$compiled_dir"/"$i"
+		tempFile=$(mktemp)
+		git diff > $tempFile
+		pushd "$targets_dir"/"$i"
+			git apply $tempFile
 		popd
+		rm $tempFile
 	popd
+
+	#pushd "$compiled_dir"
+	#	git clone "$1"/targets/"$i"/.git
+	#	pushd "$compiled_dir"/"$i"
+	#		git checkout -b skeleton
+	#		git pull # если `git remote -v` не пустой
+	#		rm -r * # скрытые файлы и директории, в том чисел и .git, никуда не деваются
+	#		$1/src/build_scripts/targets/"$i"/init.sh "$1/$2/project" "$compiled_dir"/"$i"
+	#		if ! git diff-index --quiet HEAD --
+	#		then
+	#			git add .
+	#			git commit -m"traliva: skeleton changed for target \"$i\""
+	#			git push --set-upstream origin skeleton
+	#		fi
+	#	popd
+	#popd
 
 	pushd "$targets_dir"/"$i"
 		git stash
