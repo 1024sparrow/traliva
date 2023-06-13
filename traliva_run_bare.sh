40de9bd66f5f57a116699b03fabfe5382663c0af
Boris Vasilyev <1024sparrow@gmail.com>
2023-06-13 08:20:29 +0300

    develop-v2.2

--- traliva_run_bare.sh
+++ traliva_run_bare.sh
@@ -4,6 +4,7 @@ curDir=$(dirname "$0")
 path=$(realpath $curDir)/src/project_compiled/index.html
 if [ -r $path ]
 then
+	echo "firefox --new-window file://$path"
 	firefox --new-window file://$path
 else
 	echo "compile bare first"
